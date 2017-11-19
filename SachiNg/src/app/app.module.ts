6c843eacaeb64ddb203f7d2d314132a4d87d6e8c
Toshiki Arita <tarita141@gmail.com>
2017-11-18 21:48:19 -0800

    Import Covalent Modules

--- SachiNg/src/app/app.module.ts
+++ SachiNg/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CovalentLayoutModule, CovalentStepsModule } from '@covalent/core';
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +15,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
