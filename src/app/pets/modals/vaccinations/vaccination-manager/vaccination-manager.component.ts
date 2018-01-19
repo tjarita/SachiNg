656af40202dc6157c5da47b4cb327a5f5bd0647a
Toshiki Arita <tarita141@gmail.com>
2018-01-19 00:14:36 -0800

    Testing out material datepicker

--- src/app/pets/modals/vaccinations/vaccination-manager/vaccination-manager.component.ts
+++ src/app/pets/modals/vaccinations/vaccination-manager/vaccination-manager.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as _moment from 'moment';
+import {default as _rollupMoment} from 'moment';
+import { FormControl } from '@angular/forms';
+const moment = _rollupMoment || _moment;
 
 @Component({
   selector: 'app-vaccination-manager',
@@ -11,5 +15,7 @@ export class VaccinationManagerComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  vaccinationDate =  new FormControl(moment([2017, 0, 1]));
 
 }
